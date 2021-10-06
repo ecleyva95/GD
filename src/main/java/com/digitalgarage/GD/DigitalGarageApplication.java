2861c192636da3fededf77ce13d4639009502d81
Norbe Pc <ecleyva95@gmail.com>
2021-10-06 13:47:25 -0400

    new file

---
+++ src/main/java/com/digitalgarage/GD/DigitalGarageApplication.java
@@ -0,0 +1,13 @@
+package com.digitalgarage.GD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalGarageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalGarageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalgarage/GD/DigitalGarageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalgarage.GD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalGarageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
